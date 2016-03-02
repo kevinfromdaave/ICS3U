9e6a9f623a395b8029ef8cbfd88e3a568e87859d
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-03-02 14:59:11 -0500

    Added some exclamations symbols

--- ICS3U/src/appiah/HelloWorld.java
+++ ICS3U/src/appiah/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// This prints hello world to the console
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!!!!!!!!!!!!!!!!!!");
 
 	}
 
