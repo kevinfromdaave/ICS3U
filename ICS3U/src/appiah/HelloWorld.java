abd7957456a525536c3eb250988d7962a26f0f49
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-03-02 14:44:51 -0500

    Created hello world file

---
+++ ICS3U/src/appiah/HelloWorld.java
@@ -0,0 +1,16 @@
+package appiah;
+/* 
+ * HelloWorld.java
+ * This program prints hello world to console
+ * Kevin Appiah
+ * March 2nd, 2016
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello world to the console
+		System.out.println("Hello World");
+
+	}
+
+}
