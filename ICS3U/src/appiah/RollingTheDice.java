21584014f70fb6e3f62a0c20f22403fcb85bd8a6
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-03-29 14:55:11 -0400

    This is the rolling dice program with the while loop in it

--- ICS3U/src/appiah/RollingTheDice.java
+++ ICS3U/src/appiah/RollingTheDice.java
@@ -14,43 +14,51 @@ public class RollingTheDice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		
+
 		//Variables
-		
+
 		int userDie1;
 		int userDie2;
 		int compDie1;
 		int compDie2;
 		int totalUser;
 		int totalComp;
-		
+		String playAgain = "yes";
+
 		//Calculations/Output
-		
+		while(playAgain.equalsIgnoreCase("yes")){
+
 		userDie1 = (int)(Math.random()* 6) + 1;
 		userDie2 = (int)(Math.random()* 6) + 1;
 		totalUser = userDie1 + userDie2;
-										
+
 		System.out.println("Your number is " + totalUser);
 
 		compDie1 = (int)(Math.random()* 6) + 1;
 		compDie2 = (int)(Math.random()* 6) + 1;
 		totalComp = compDie1 + compDie2;
-		
+
 		System.out.println("My number is " + totalComp);
-		
+
 		//Decisions
+
 		
-		if (totalUser > totalComp){
-			System.out.println("That means you win :(");
-		}
-		 
-		else if (totalUser < totalComp){
-			System.out.println("That means I win :)");
-		}
-		
-		else{
-			System.out.println("It's a tie try again");
+			if (totalUser > totalComp){
+				System.out.println("That means you win :(");
+			}
+
+			else if (totalUser < totalComp){
+				System.out.println("That means I win :)");
+			}
+
+			else {
+				System.out.println("It's a tie.");					
+			}
+
+			System.out.println("Play again?");
+			playAgain = scan.nextLine();
+			
+			
 		}
 	}
-
 }
