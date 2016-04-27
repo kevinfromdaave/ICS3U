7b24a5f4ae6357941648e1d08729c78834aaa767
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-04-27 14:46:49 -0400

    Happy and you know it song

---
+++ ICS3U/src/appiah/IfYoureHappy.java
@@ -0,0 +1,27 @@
+package appiah;
+
+import java.util.Scanner;
+
+public class IfYoureHappy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+		int verse = 10;
+		
+		String[] lyrics = new String[] {"clap your hands", "jump around", "get a friend", "eat some food", "get good grades", "shoot some hoops", "play football", "party hard", "have some fun", "stomp around"};
+		
+		for (int i = 0; i<verse; i++){
+			System.out.println("If you're happy and you know it " + lyrics[i]);
+			System.out.println("If you're happy and you know it " + lyrics[i]);
+			System.out.println("If you're happy and you know and you really want to show it, if you're happy and you know it " + lyrics[i]);
+			System.out.println("If you're happy and you know it " + lyrics[i]);
+			System.out.println(" ");
+			
+		}
+		
+
+	}
+
+}
