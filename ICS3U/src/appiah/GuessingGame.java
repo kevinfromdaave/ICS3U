03d619d4ecd1a2af2c8ac6c2b6dad66545591abe
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-04-28 15:05:39 -0400

    This program isn't complete. I will finish tomorrow

---
+++ ICS3U/src/appiah/GuessingGame.java
@@ -0,0 +1,23 @@
+package appiah;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] animals = {"Lion", "Tiger", "Antelope", "Crocodile", "Ape", "Dog", "Leopard", "Bear", "Wolf", "Giraffe", "Flamingo", "Rhino", "Cat", "Zebra", "Horse", "Hyena", "Sheep", "Owl", "Snake", "Donkey"};
+		boolean[] animal = new boolean[20];
+		String rightWrong;
+		int c = 0;
+		System.out.println("Think of an animal, and I'll guess");
+		
+		for (int i = 20; i!= -1; i--){
+			c++;
+			int guess = (int)(Math.random()*)
+			
+		}
+		
+
+	}
+
+}
