a32bafbd35f669c61913876dc1bd740ad539fc4b
323464446 <323464446@C-GD23VR1.tdsb.on.ca>
2016-05-18 14:53:39 -0400

    This program spells out the word Mississipi

---
+++ ICS3U/src/appiah/mississipi.java
@@ -0,0 +1,107 @@
+package appiah;
+/**
+ * Mississippi.java
+ * @author 323464446
+ *
+ */
+
+public class mississipi {
+	
+	/**
+	 * This main method prints out Mississippi
+	 * @param args
+	 */
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		drawM();
+		System.out.println(" ");		
+		drawI();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");		
+		drawS();
+		System.out.println(" ");
+		drawI();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");	
+		drawS();
+		System.out.println(" ");		
+		drawI();
+		System.out.println(" ");		
+		drawP();
+		System.out.println(" ");		
+		drawP();
+		System.out.println(" ");		
+		drawI();		
+
+	}
+	
+	/**
+	 * This method draws P
+	 * 
+	 */
+
+	private static void drawP() {
+		// TODO Auto-generated method stub
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+						
+	}
+	
+	/**
+	 * This method draws S
+	 */
+	private static void drawS() {
+		// TODO Auto-generated method stub
+		System.out.println(" SSSSSS ");
+		System.out.println("S      S");
+		System.out.println("S       ");
+		System.out.println(" SSSSSS ");
+		System.out.println("       S");
+		System.out.println("S      S");
+		System.out.println(" SSSSSS ");		
+		
+	}
+	
+	/**
+	 * This method draws I
+	 */
+
+	private static void drawI() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+		
+		
+	}
+	
+	/**
+	 * This method draws M
+	 */
+
+	private static void drawM() {
+		// TODO Auto-generated method stub
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		
+	}
+
+}
